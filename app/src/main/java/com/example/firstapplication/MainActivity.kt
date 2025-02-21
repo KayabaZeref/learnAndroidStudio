36821ac09661140b09499652f337bd7d227aa5ce
KayabaZeref <duyphuong2000.dpp@gmail.com>
2025-02-21 13:58:09 +0700

    ``` feat: Add .idea project configuration files
    This commit introduces initial project configuration files under the `.idea` directory. These files define project settings for the
    ``````
     IDE and include:
    
    -   `vcs.xml`: Maps the project directory to Git version control.
    -   `appInsightsSettings.xml`: Configures Application Insights settings with placeholder values.
    -   `misc.xml`: Specifies external storage configuration, project SDK, and project type.
    -
    ``````
    `kotlinc.xml`: Sets the Kotlin JPS plugin version.
    -   `gradle.xml`: Configures Gradle project settings, including external project path and module information.
    -   `inspectionProfiles/Project_Default.xml`: Defines default inspection settings, with specific focus on Compose and Glance preview validation.
    -   `migrations.xml`: Sets the option to migrate to Gradle local Java Home.
    -   `runConfigurations.xml`: Manages ignored producers of run configurations.
    -   `.gitignore`: Specifies files within `.idea` that should be ignored by Git.
    -   `codeStyles/Project
    ``````
    .xml`: Configures code style settings for XML and Kotlin.
    -   `deploymentTargetSelector.xml`: Configures deployment target selection states.
    -   `codeStyles/codeStyleConfig.xml`: Enables per-project code style settings.
    -   `compiler.xml`: Sets bytecode target level to 21.
    
    These files establish initial IDE project settings and ensure consistency across development environments.
    ```

---
+++ app/src/main/java/com/example/firstapplication/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.example.firstapplication
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.firstapplication.ui.theme.FirstApplicationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            FirstApplicationTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Surface(color = Color.Cyan)  {
+        Text(
+            text = "Hi, my name is $name!",
+            modifier = modifier.padding(24.dp)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    FirstApplicationTheme {
+        Greeting("Zeref")
+    }
+}
