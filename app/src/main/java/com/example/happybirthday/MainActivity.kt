fd6b6409a4a621392ebee6e19490072732388e90
KayabaZeref <duyphuong2000.dpp@gmail.com>
2025-02-26 19:40:24 +0700

    Update code

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -16,9 +16,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -53,16 +56,19 @@ fun GreetingText(message: String, form: String, modifier: Modifier = Modifier) {
     ) {
         Text(
             text = message,
-            fontSize = 100.sp,
+            fontSize = 80.sp,
             lineHeight = 116.sp,
             textAlign = TextAlign.Center,
-           )
+            fontWeight = FontWeight.Bold,
+            color = Color(0xFF29710B),
+        )
         Text(
             text = form,
             fontSize = 36.sp,
             modifier = Modifier
                 .padding(16.dp)
-                .align(alignment = Alignment.CenterHorizontally)
+                .align(alignment = Alignment.CenterHorizontally),
+            color = Color(0xFF0F8693)
         )
     }
 }
@@ -93,7 +99,8 @@ fun BirthdayCardPreview() {
     HappyBirthdayTheme {
         GreetingImage(
             message = "Happy Birthday Sam!",
-            form = "From Emma",)
+            form = "From Emma",
+        )
 
     }
 }
